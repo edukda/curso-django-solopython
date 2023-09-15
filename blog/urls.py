46b78254fc80694442d04ee9c08a84e198582e13
edukda <132685638+edukda@users.noreply.github.com>
2023-09-15 19:48:18 -0400

    funciones del formulario

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import BlogView
+from .views import BlogView, BlogViewCreate, BlogViewList
 
 urlpatterns = [
-    path('', BlogView.as_view())
+    path('', BlogView.as_view()),
+    path('create/', BlogViewCreate.as_view(), name='create'),
+    path('list/', BlogViewList.as_view(), name='list')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_list_or_404  
 from django.views.generic import View
+from .form import CreateForm
+from .models import Post
 
 class BlogView(View):
     def get(self, request, *args, **kwargs):
@@ -7,17 +9,38 @@ class BlogView(View):
         }
         return render(request, "blog.html", context)
     
-class BlogViewPost(View):
+class BlogViewList(View):
     def get(self, request, *args, **kwargs):
-        context = {
 
+        posts = Post.objects.all()
+        context = {
+            'posts': posts
+        }
+
+        return render(request, 'blog_list.html', context)
+    
+class BlogViewCreate(View):
+    def get(self, request, *args, **kwargs):
+        form = CreateForm()
+        context = {
+            'form': form
         }
         return render(request, "form.html", context)
     
     def post(self, request, *args, **kwargs):
-        context = {
+        if request.method == "POST":
+            form = CreateForm(request.POST)
+            if form.is_valid():
+                title = form.cleaned_data.get('title')
+                comment = form .cleaned_data.get('comment')
+
+                p, created = Post.objects.get_or_create(title=title, comment=comment)
+                p.save()
+                return redirect('primary')
             
+        context={
+
         }
-        return render(request, "form.html", context)        
+        return render(request, "form.html", context)     
 
 # Create your views here.
